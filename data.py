91b082b113d0d8bfacbe705742ed09d066dbcb3a
RynCav <ryancav101@gmail.com>
2024-09-07 15:53:42 -0400

    Update data.py
    Added Validation sets

--- data.py
+++ data.py
@@ -1,17 +1,21 @@
 from utils import load, randomize_lists
-
-
+#inherited class, template
 class Data:
     #initialize the data
     def __init__(self):
         #load files, values are already normilized & y_true is onehot encoded
         self.train_X, self.test_X = load(f'{self.directory}Train_X'), load(f'{self.directory}Test_X')
         self.train_y, self.test_y = load(f'{self.directory}Train_y'), load(f'{self.directory}Test_y')
+        if self.valuation_set:
+            self.evaluate_y, self.evaluate_y = load(f'{self.directory}Evaluate_y'), load(f'{self.directory}Evaluate_y')
 
     #load the training data
     def train(self):
         # Randomises the data
         return randomize_lists(self.train_X, self.train_y)
+
+    def evaluate(self):
+        return self.evaluate_X, self.evaluate_y
 
     #load the testing data
     def test(self):
@@ -22,8 +26,10 @@ class Data:
 #standerd MNIST dataset
 class MNIST(Data):
     directory = 'Datasets/MNIST/MNIST_'
+    valuation_set = False
 
 
 #Fashion MNIST dataset
 class FashionMNIST(Data):
     directory = 'Datasets/Fashion MNIST/FMNIST_'
+    valuation_set = False
