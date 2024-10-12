bdd9824da928b447a7cea80cb43c2e97d5da2b2b
RynCav <ryancav101@gmail.com>
2024-10-12 19:41:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 from params import *
 from model import *
 
-#innizlize each layer and activation function
+# innizlize each layer and activation function
 STEPS = [
-   Dense_Layer(784, 100), ReLU(), Dropout_Layer(dr), Dense_Layer(100, 64), ReLU(), Dropout_Layer(dr),
-   Dense_Layer(64, 10), Softmax()
+   DenseLayer(784, 100), ReLU(), DropoutLayer(dr), DenseLayer(100, 64), ReLU(), DropoutLayer(dr),
+   DenseLayer(64, 10), Softmax()
 ]
 
-#create the model object
-model = Model(STEPS)
+# create the model object
+model = Model(STEPS, scheduler = OneCycleLR(max_lr, lr, EPOCHS, BATCH_SIZE, Dataset.size))
 
-#train the model
-model.train(dataset, EPOCHS, BATCH_SIZE)
+# train the model
+model.train(Dataset, EPOCHS, BATCH_SIZE)
 
-#test the model to determine accuracy
-model.test(dataset)
+# test the model to determine accuracy
+model.test(Dataset)
 
-#save the model to the specified file1
+# save the model to the specified file1
 save(model, 'model.pickle')
