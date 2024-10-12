c9202bf1ac2eb5ccec67410e551c6ab2c44322f3
RynCav <ryancav101@gmail.com>
2024-10-12 19:42:20 -0400

    Update params.py

--- params.py
+++ params.py
@@ -1,12 +1,13 @@
 from data import MNIST
 
-#Hyperparameters
+# Hyperparameters
 BATCH_SIZE = 100
 EPOCHS = 10
 lr = 0.0001
+max_lr = 0.001
 dr = 1
 DECAY = 5e-5
 PATIENCE = 3
 
-#Dataset used:
-dataset = MNIST()
+# Dataset used:
+Dataset = MNIST()
